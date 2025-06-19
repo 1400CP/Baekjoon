cd021d25dae79d2abb7ad529f24640fb1f6af672
1400CP <bugyjp101@gmail.com>
2025-06-20 01:26:15 +0900

    20250620

--- Baekjoon/src/com/baekjoon/test2/Main2.java
+++ Baekjoon/src/com/baekjoon/test2/Main2.java
@@ -12,7 +12,8 @@ public class Main2 {
 		Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        sc.close();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
         
         if(A > B){
             System.out.println(">");
@@ -34,6 +35,51 @@ public class Main2 {
         }else if(C == D){
             System.out.println("==");
         }
+        
+        // 9498번
+        if(A >= 90){
+            System.out.println("A");
+        }else if(A >= 80){
+            System.out.println("B");
+        }else if(A >= 70){
+            System.out.println("C");
+        }else if(A >= 60){
+            System.out.println("D");
+        }else {
+            System.out.println("F");
+        }
+        
+        // System.out.print((A>=90) ? "A" : (A>=80) ? "B" : (A>=70) ? "C" : (A>=60) ? "D" : "F");
+        
+        // 2753번
+        if((A%4 == 0) && (A%100 != 0 || A%400 == 0)){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+
+        // 14681번
+
+        if(x > 0 && y > 0){
+            System.out.println("1");
+        }else if(x > 0 && y < 0){
+            System.out.println("4");
+        }else if(x < 0 && y < 0){
+            System.out.println("3");
+        }else if(x < 0 && y > 0){
+            System.out.println("2");
+        }
+
+        if(x > 0 && y > 0){
+            System.out.println("1");
+        }else if (x < 0 && y > 0){
+            System.out.println("2");
+        }else if (x < 0 && y < 0){
+            System.out.println("3");
+        }else if (x > 0 && y < 0){
+            System.out.println("4");
+        }
+
 
 	}
 
