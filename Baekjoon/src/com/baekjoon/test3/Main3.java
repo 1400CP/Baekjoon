af9c70740b92e02b0ecb7d76cde0c6cabbda7191
1400CP <bugyjp101@gmail.com>
2025-06-29 22:06:36 +0900

    Update Main3.java

--- Baekjoon/src/com/baekjoon/test3/Main3.java
+++ Baekjoon/src/com/baekjoon/test3/Main3.java
@@ -172,8 +172,37 @@ public class Main3 {
         bw.close();
 
         // 2438
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=i; j++){
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
         
 
+        // 2439
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N-i; j++){
+                bw.write(" ");
+            }
+            for(int k=1; k<=i; k++){
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
 
     }    
 }
