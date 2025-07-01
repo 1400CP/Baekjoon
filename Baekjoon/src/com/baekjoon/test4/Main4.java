66b4aadd29d506c30abfaad4f84d2c4f5f44184b
1400CP <bugyjp101@gmail.com>
2025-07-01 21:20:37 +0900

    20250701

--- Baekjoon/src/com/baekjoon/test4/Main4.java
+++ Baekjoon/src/com/baekjoon/test4/Main4.java
@@ -5,10 +5,42 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main4 {
     public static void main(String[] args) throws IOException{
+
+        // 10807번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N >= 1 && N <= 100){
+            int[] arr = new int[N];
+            int count = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            for(int i=0; i<N; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int a = Integer.parseInt(br.readLine());
+
+            for(int j=0; j<arr.length; j++){
+                if(a == arr[j]){
+                    count++;
+                }
+            }
+            System.out.println(count); // bw.write는 String형이기 때문에 int 불가.
+
+            br.close();
+            bw.flush();
+            bw.close();
+        }
+
+        
+        
     }
 }
