2f9b56a2f6d466d75f7d7ac60c6aa697f224fff2
1400CP <bugyjp101@gmail.com>
2025-08-03 05:42:23 +0900

    Update Main5.java

--- Baekjoon/src/com/baekjoon/test5/Main5.java
+++ Baekjoon/src/com/baekjoon/test5/Main5.java
@@ -2,9 +2,11 @@ package com.baekjoon.test5;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main5 {
     public static void main(String[] args){
+
         // 27866번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine(); // 글자 주어짐
@@ -13,11 +15,13 @@ public class Main5 {
         System.out.print(S.charAt(I-1)); // 0번부터 시작하기 때문에 -1 추가
         // charAt 메소드를 통해 글자 추출.
 
+
         // 2743번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         
         System.out.println(s.length());
+
 
         // 9086번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,6 +34,7 @@ public class Main5 {
         }
         br.close();
 
+
         // 11654번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
@@ -37,6 +42,7 @@ public class Main5 {
         br.close();
         
         System.out.println(i);
+
 
         // 11720번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -51,6 +57,7 @@ public class Main5 {
         br.close();
         System.out.println(sum); // 최종적으로 더해진 값을 출력.
 
+
         // 10809번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder(); // 객체 선언. append() 메소드로 추가.
@@ -61,6 +68,7 @@ public class Main5 {
         }
         
         System.out.print(sb);
+
 
         // 2675번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -81,11 +89,39 @@ public class Main5 {
         }
         br.close(); // 이걸로 br.readLine()의 마지막을 닫음. 먼저 쓰면 향상문 1번으로 끝이 나버림.
 
+
         // 1152번
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " "); // Tokenizer 사용법
         
         System.out.println(st.countTokens()); // countTokens()로 세는 법 알아두기.
+
+
+        // 2908번-1
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] arr = br.readLine().split(" "); // 주어지는 세자리 숫자를 배열로
+        
+        br.close();
+        
+        int a = Integer.parseInt(new StringBuilder().append(arr[0]).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder().append(arr[1]).reverse().toString());
+        // StringBuilder에 대해 다시 공부
+
+        System.out.println(a>b?a:b); // 삼항 연산자로 첫번째 숫자가 나오도록 함
+
+        // 2908번-2
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        // 배열로 나타낼 것을 StringTokenizer로 나타내기
+
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        // 위의 append(arr[])을 st.nextToken() - 다음 토큰 값으로 대체하여 나타냄.
+
+        br.close();
+        
+        System.out.println(a>b?a:b); // 삼항 연산자로 첫번째 숫자가 나오도록 함
 
     }
 }
