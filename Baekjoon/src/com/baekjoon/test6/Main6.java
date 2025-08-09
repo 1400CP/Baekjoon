369806ae19b460565c2603195c8ecf7ab157f5a5
1400CP <bugyjp101@gmail.com>
2025-08-09 11:02:10 +0900

    Create Main6.java

---
+++ Baekjoon/src/com/baekjoon/test6/Main6.java
@@ -0,0 +1,26 @@
+package com.baekjoon.test6;
+
+public class Main6 {
+    public static void main(String[] args){
+        //25083번-1
+        String s = "         ,r'\"7" +
+                   "r`-_   ,'  ,/" +
+                   " \\. \". L_r'" +
+                   "   `~\\/" +
+                   "      |\n" +
+                   "      |";
+        System.out.print(s);
+
+        //25083번-2
+        System.out.print("         ,r'\"7\n");
+        System.out.print("r`-_   ,'  ,/\n");
+        System.out.print(" \\. \". L_r'\n");
+        System.out.print("   `~\\/\n");
+        System.out.print("      |\n");
+        System.out.print("      |\n");
+
+        //3003번
+        
+
+    }
+}
