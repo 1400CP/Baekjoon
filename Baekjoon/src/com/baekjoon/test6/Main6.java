2bab44b97adcfce81542fca645ca8b825f32b144
1400CP <bugyjp101@gmail.com>
2025-08-12 15:04:41 +0900

    Update Main6.java

--- Baekjoon/src/com/baekjoon/test6/Main6.java
+++ Baekjoon/src/com/baekjoon/test6/Main6.java
@@ -1,5 +1,9 @@
 package com.baekjoon.test6;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main6 {
     public static void main(String[] args){
         //25083번-1
@@ -20,6 +24,30 @@ public class Main6 {
         System.out.print("      |\n");
 
         //3003번
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int king = 1;
+        int queen = 1;
+        int rook = 2;
+        int bishop = 2;
+        int knight = 2;
+        int pawn = 8;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        king = king - Integer.parseInt(st.nextToken());
+        queen = queen - Integer.parseInt(st.nextToken());
+        rook = rook - Integer.parseInt(st.nextToken());
+        bishop = bishop - Integer.parseInt(st.nextToken());
+        knight = knight - Integer.parseInt(st.nextToken());
+        pawn = pawn - Integer.parseInt(st.nextToken());
+        
+        System.out.print(king + " ");
+        System.out.print(queen + " ");
+        System.out.print(rook + " ");
+        System.out.print(bishop + " ");
+        System.out.print(knight + " ");
+        System.out.print(pawn + " ");
+
+        //2444번
         
 
     }
