01d5976587d930eb79610edc0b62818a84bb0be9
1400CP <bugyjp101@gmail.com>
2025-08-12 18:38:14 +0900

    Update Main6.java

--- Baekjoon/src/com/baekjoon/test6/Main6.java
+++ Baekjoon/src/com/baekjoon/test6/Main6.java
@@ -48,6 +48,34 @@ public class Main6 {
         System.out.print(pawn + " ");
 
         //2444번
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<N-i; j++){
+                sb.append(" ");
+            }
+            for(int j=0; j<(2*i)-1; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        
+        for(int i=N-1; i>=1; i--){
+            for(int j=0; j<N-i; j++){
+                sb.append(" ");
+            }
+            for(int j=0; j<(2*i)-1; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        br.close();
+        System.out.print(sb);
+
+        //10988번
         
 
     }
