cbfefc058efbd298ddb8bc3b4ae375cf2fa4b9a8
1400CP <bugyjp101@gmail.com>
2025-11-04 23:34:05 +0900

    Update main9.java

--- Baekjoon/src/com/baekjoon/test9/main9.java
+++ Baekjoon/src/com/baekjoon/test9/main9.java
@@ -27,5 +27,26 @@ public class Main{
         br.close();
 
         // 2501번
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int count = 0; // 결과값을 측정하기 위한 값을 지정함.
+        for(int i=1; i<=N; i++){ // 약수의 값만큼 향상문을 해야 함. N번 지정해야 함.
+            if(N%i==0){ // i번 필요하고 약수는 나머지 값이 0이 나와야 함.
+                count++; // 약수를 측정하기 위한 count가 한번씩 카운트 되도록 함.
+                if(count == K){ // K 값과 같아지도록 함.
+                    System.out.println(i); // i값과 count값이 하나씩 같이 올라감.
+                    return; // 향상문이 멈추도록 break 또는 return함.
+                }
+            }
+        }
+        System.out.println(0); // 향상문에서 약수값을 구했기 때문에 이외의 값은 0이 나오도록 함.
+
+        // 9506번
+        
+
     }
 }
