5afece13ff2c8793410d4a26fbd7bfbf6d35bc1b
lyh <asgard@asgard.com>
2025-11-25 23:49:47 +0900

    Update main9.java

--- Baekjoon/src/com/baekjoon/test9/main9.java
+++ Baekjoon/src/com/baekjoon/test9/main9.java
@@ -46,7 +46,45 @@ public class Main{
         System.out.println(0); // 향상문에서 약수값을 구했기 때문에 이외의 값은 0이 나오도록 함.
 
         // 9506번
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            int n = Integer.parseInt(br.readLine());
+            if(n == -1){
+                break;
+            }
+            
+            int[] arr = new int[n];        // 배열, 약수의 배열
+            int sum = 0;        // 약수들의 합
+            int index = 0;        // 약수를 담을 인덱스
+            
+            for(int i=1; i<n; i++){
+                if(n%i == 0){        // 약수일 때를 가정
+                    arr[index++] = i;        // 약수를 배열에 저장
+                    sum += i;        // 약수의 합을 계산
+                }
+            }
+            
+            if(sum != n){        // 약수의 합이 n과 다를 경우 (완전수가 아님)
+                System.out.println(n + " is NOT perfect.");
+                continue;
+            }
+            
+            System.out.print(n + " = "); // 완전수 앞부분 출력
+            
+            for(int i=0; i<index; i++){ // 배열에 저장된 약수를 출력
+                if(i == index-1){        // 약수를 출력해야 하는데, 자신이 값인 경우를 제외하기 위하여(?)
+                    System.out.print(arr[i]);
+                }else{        // 약수를 끝까지 출력함.
+                    System.out.print(arr[i] + " + ");
+                }
+            }
+            System.out.println();
+        }
+        br.close();
+
+
+        // 1978번
 
     }
 }
